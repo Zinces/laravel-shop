f88f1b3c2b0bf053b870edd2fb52d955a051c1bc
zinces <305977606@qq.com>
2019-03-01 08:30:48 +0000

    收货地址列表

---
+++ resources/views/errors/_message.blade.php
@@ -0,0 +1,12 @@
+<!-- 输出后端报错开始 -->
+
+@if (count($errors) > 0)
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li><i class="glyphicon glyphicon-remove"></i> {{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+<!-- 输出后端报错结束 -->
