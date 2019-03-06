d83d2d2a1b78112899c36a91d727a394adc58bdf
zinces <305977606@qq.com>
2019-03-06 03:40:20 +0000

    收藏商品列表

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,6 @@
 //Route::get('/', 'PagesController@root')->name('root');
 Route::redirect('/', '/products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
-Route::get('products/{product}', 'ProductsController@show')->name('products.show');
 
 
 Auth::routes();
@@ -35,8 +34,9 @@ Route::group(['middleware' => 'auth'], function() {
 
         Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
         Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
-
+        Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
     });
     // 结束
 });
+Route::get('products/{product}', 'ProductsController@show')->name('products.show')->where(['product' => '[0-9]+']);;
 
